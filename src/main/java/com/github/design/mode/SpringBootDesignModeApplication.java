41fd234406ef865fa585c6e34f5bb695a98bdc5a
shaopro <shaopro@qq.com>
2020-06-10 19:37:07 +0800

    Initial commit

---
+++ src/main/java/com/github/design/mode/SpringBootDesignModeApplication.java
@@ -0,0 +1,13 @@
+package com.github.design.mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesignModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDesignModeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/design/mode/SpringBootDesignModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.design.mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesignModeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
