70b229e622deaa78fc0e4ae234f5145f45b71108
shaopro <shaopro@qq.com>
2020-06-10 22:30:09 +0800

    模板方法模式

---
+++ src/main/java/com/github/design/mode/template/BaseTemplateImpl.java
@@ -0,0 +1,19 @@
+package com.github.design.mode.template;
+
+/**
+ * @author 石少东
+ * @date 2020-06-10 22:13
+ */
+
+public class BaseTemplateImpl extends BaseTemplate {
+
+    @Override
+    public void method2() {
+        System.out.println("BaseTemplateImpl - method2");
+    }
+
+    @Override
+    public void method3() {
+        System.out.println("BaseTemplateImpl - method3");
+    }
+}
---
+++ src/main/java/com/github/design/mode/template/Main.java
@@ -0,0 +1,16 @@
+package com.github.design.mode.template;
+
+/**
+ * @author 石少东
+ * @date 2020-06-10 22:13
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        BaseTemplate baseTemplate = new BaseTemplateImpl();
+        baseTemplate.kickOff();
+
+    }
+
+}
